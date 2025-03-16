e3c0988816f38697394ab9978f897ea98afa18f5
MaxH@windows10 <johnsonheo96@gmail.com>
2025-03-17 01:33:27 +0900

    윈도우 exe 파일 배포

---
+++ build/exe.win-amd64-3.11/build_and_run.bat
@@ -0,0 +1,50 @@
+@echo off
+
+:: utf-8 코드페이지 설정 (한글 깨짐 현상 수정)
+chcp 65001 >nul
+
+:: REM 스크립트가 있는 디렉토리로 이동
+cd /d "%~dp0"
+
+echo Current Directory: %cd%
+
+:: REM Python 설치 확인
+where python >nul 2>&1
+if %errorlevel% neq 0 (
+    echo [ERROR] Python이 설치되지 않았습니다. https://www.python.org/ftp/python/3.13.2/python-3.13.2-amd64.exe 에서 받은 exe파일로 설치 후 다시 시도해주세요.
+    exit /b 1
+) else (
+    echo [INFO] Python이 설치되어 있습니다.
+)
+
+:: REM 가상환경(venv) 생성
+if not exist "venv" (
+    echo [INFO] 가상환경^(venv^) 생성 중...
+    python -m venv venv
+)
+
+:: REM 가상환경 활성화
+call venv\Scripts\activate
+
+:: REM QT 플러그인 경로 설정
+set QT_QPA_PLATFORM_PLUGIN_PATH=venv\Lib\site-packages\PyQt6\Qt6\plugins\platforms
+set QT_PLUGIN_PATH=venv\Lib\site-packages\PyQt6\Qt6\plugins
+
+:: REM pip 업그레이드
+(
+    echo [INFO] pip 업그레이드 중...
+    python -m pip install --upgrade pip
+)
+
+:: REM requirements.txt 기반 패키지 설치
+if exist "requirements.txt" (
+    echo [INFO] requirements.txt 기반 패키지 설치 중...
+    pip install -r requirements.txt
+) else (
+    echo [ERROR] requirements.txt 파일을 찾지 못했습니다.
+    exit /b 1
+)
+
+:: REM 프로그램 실행
+echo [INFO] 프로그램 실행 중...
+python main.py
--- setup_win.py
+++ setup_win.py
@@ -1,6 +1,7 @@
 # 윈도우 Python 3.12 이상 버전에서 cx_Freeze import 시 import msilib 오류남 
 # 윈도우 환경에서 cx_Freeze를 ModuleError 없이 구동시키려면 시스템에 Python 3.11 설치 후 venv 설치
 # venv 설정 후 terminal에서 python setup_win.py build_exe 실행하면 build 파일 아래 exe가 만들어짐
+
 from cx_Freeze import setup, Executable
 import os
 
