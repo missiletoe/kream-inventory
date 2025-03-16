034521f439ab47e24e76aa13d165e37f207a5cae
MaxH@windows10 <johnsonheo96@gmail.com>
2025-03-16 23:16:34 +0900

    윈도우에서 실행 가능하도록 브라우저 환경변수 설정 및 batch 파일 수정

--- browser_manager.py
+++ browser_manager.py
@@ -1,18 +1,33 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
 import chromedriver_autoinstaller
+import sys
+import os
 
 class BrowserManager:
     def __init__(self):
         options = webdriver.ChromeOptions()
-        user_agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36'  # 사용자 에이전트 설정
-        options.add_argument(f'user-agent={user_agent}')  # 사용자 에이전트 옵션 추가
-        options.add_argument("--headless")  # UI 없이 실행
+
+        # user-agent 설정 (25년 3월 기준 크롬 134 버전 / CSR 페이지 크롤링 시 설정필수)
+        if sys.platform == 'darwin':
+            user_agent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.0.0 Safari/537.36' 
+        elif sys.platform == 'win32':
+            user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/134.0.0.0 Safari/537.36'
+            # 윈도우에서 크롬 실행파일 경로 지정
+            chrome_path = r"C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe"
+            if not os.path.exists(chrome_path):
+                chrome_path = r"C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe"
+            options.binary_location = chrome_path
+
+        options.add_argument(f'user-agent={user_agent}')
+        options.add_argument("--headless")  # 크롬 창 숨기기
         options.add_argument("--disable-gpu")
         options.add_argument("--no-sandbox")
         options.add_argument("--disable-dev-shm-usage")
 
-        chromedriver_autoinstaller.install()
-        self.driver = webdriver.Chrome(options=options)
+        driver_path = chromedriver_autoinstaller.install()
+        service = Service(driver_path)
+        self.driver = webdriver.Chrome(service=service, options=options)
 
     def get_browser(self):
         return self.driver
--- build_and_run.bat
+++ build_and_run.bat
@@ -1,30 +1,50 @@
 @echo off
-setlocal
 
-:: 1️⃣ Python이 설치되어 있는지 확인
-where python >nul 2>nul
+:: utf-8 코드페이지 설정 (한글 깨짐 현상 수정)
+chcp 65001 >nul
+
+:: REM 스크립트가 있는 디렉토리로 이동
+cd /d "%~dp0"
+
+echo Current Directory: %cd%
+
+:: REM Python 설치 확인
+where python >nul 2>&1
 if %errorlevel% neq 0 (
-    echo [INFO] Python이 설치되지 않았습니다.
-    echo [INFO] Python을 설치해야 합니다: https://www.python.org/downloads/windows/
-    pause
-    exit /b
+    echo [ERROR] Python이 설치되지 않았습니다. https://www.python.org/ftp/python/3.13.2/python-3.13.2-amd64.exe 에서 받은 exe파일로 설치 후 다시 시도해주세요.
+    exit /b 1
+) else (
+    echo [INFO] Python이 설치되어 있습니다.
 )
 
-:: 2️⃣ 가상 환경 생성
-if not exist venv (
-    echo [INFO] 가상 환경(venv) 생성 중...
+:: REM 가상환경(venv) 생성
+if not exist "venv" (
+    echo [INFO] 가상환경^(venv^) 생성 중...
     python -m venv venv
 )
 
-:: 3️⃣ 가상 환경 활성화
+:: REM 가상환경 활성화
 call venv\Scripts\activate
 
-:: 4️⃣ pip 업그레이드
-echo [INFO] pip 업그레이드 중...
-python -m pip install --upgrade pip
+:: REM QT 플러그인 경로 설정
+set QT_QPA_PLATFORM_PLUGIN_PATH=venv\Lib\site-packages\PyQt6\Qt6\plugins\platforms
+set QT_PLUGIN_PATH=venv\Lib\site-packages\PyQt6\Qt6\plugins
 
-:: 5️⃣ 패키지 설치
-pip install -r requirements.txt
+:: REM pip 업그레이드
+(
+    echo [INFO] pip 업그레이드 중...
+    python -m pip install --upgrade pip
+)
 
-:: 6️⃣ UI 프로그램 실행
+:: REM requirements.txt 기반 패키지 설치
+if exist "requirements.txt" (
+    echo [INFO] requirements.txt 기반 패키지 설치 중...
+    pip install -r requirements.txt
+) else (
+    echo [ERROR] requirements.txt 파일을 찾지 못했습니다.
+    exit /b 1
+)
+
+:: REM 프로그램 실행
+echo [INFO] 프로그램 실행 중...
 python main.py
